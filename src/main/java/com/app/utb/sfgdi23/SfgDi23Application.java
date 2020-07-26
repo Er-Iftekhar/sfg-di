112a856d95dabf35c4d124f9f14622bb3c418f57
SyedIftekharuddin <syedy636@gmail.com>
2020-07-26 18:41:09 +0200

    Initial project add

---
+++ src/main/java/com/app/utb/sfgdi23/SfgDi23Application.java
@@ -0,0 +1,13 @@
+package com.app.utb.sfgdi23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDi23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDi23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/utb/sfgdi23/SfgDi23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.utb.sfgdi23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDi23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
